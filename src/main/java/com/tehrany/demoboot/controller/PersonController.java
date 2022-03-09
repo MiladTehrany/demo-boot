de79bf06ef165d43a3e0514f54ba6435586fad51
Milad Tehrany <mhtxpress7@gmail.com>
2022-03-09 10:53:22 +0330

    Change functionality

--- src/main/java/com/tehrany/demoboot/controller/PersonController.java
+++ src/main/java/com/tehrany/demoboot/controller/PersonController.java
@@ -1,9 +1,9 @@
 package com.tehrany.demoboot.controller;
 
-import com.tehrany.demoboot.model.entity.Car;
 import com.tehrany.demoboot.model.entity.Person;
 import com.tehrany.demoboot.model.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +16,8 @@ public class PersonController {
     private PersonService personService;
 
     @PostMapping("/save")
-    public void save(@RequestBody Person person) {
-        try {
-            System.out.println(person.getFamily());
-            System.out.println(person.getName());
-            System.out.println(person.getCars().size());
-            for (Car car : person.getCars()) {
-                System.out.println(car.getModel());
-            }
-            //personService.register(person);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public ResponseEntity<Person> save(@RequestBody Person person) {
+        personService.register(person);
+        return ResponseEntity.ok(person);
     }
 }
